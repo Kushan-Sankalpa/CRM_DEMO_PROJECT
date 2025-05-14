a4eac9e3deb4952d6b0ef590ad4801d1b1298fea
kushan-panda <kushan.20230023@iit.ac.lk>
2025-05-14 22:10:07 +0530

    Update dashboard.blade.php

--- laravel-crm/resources/views/dashboard.blade.php
+++ laravel-crm/resources/views/dashboard.blade.php
@@ -9,7 +9,9 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                    <h3>Welcome, {{ Auth::user()->name }}!</h3>
+                    <p>Email: {{ Auth::user()->email }}</p>
+                    <a href="{{ route('profile.edit') }}" class="text-blue-500">Edit Profile</a>
                 </div>
             </div>
         </div>
