4345b6eed27f844224991a035a365906bfb60509
kushan-panda <kushan.20230023@iit.ac.lk>
2025-05-15 09:09:35 +0530

    Finalized code files

--- laravel-crm/routes/web.php
+++ laravel-crm/routes/web.php
@@ -8,32 +8,32 @@ use App\Http\Controllers\ProposalController;
 use App\Http\Controllers\TransactionController;
 
 
-
-Route::get('/', function () {
+//login page
+Route::get('/', function () { 
    return redirect('/login');
 });
 
-Route::post('/logout', function () {
-    Auth::logout();
-    return redirect('/login');
-})->name('logout');
-
+//dashboard page(navigate only  after login)
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+//profile page
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+//customer page routes
 Route::resource('customers', CustomerController::class)->middleware(['auth']);
 Route::post('customers/{customer}/status', [CustomerController::class, 'updateStatus'])->name('customers.updateStatus')->middleware(['auth']);
 
+//proposal page routes
 Route::resource('proposals', ProposalController::class)->middleware(['auth']);
 Route::post('proposals/{proposal}/status', [ProposalController::class, 'updateStatus'])->name('proposals.updateStatus')->middleware(['auth']);
 
+//invoice page routes
 Route::resource('invoices', InvoiceController::class)->middleware(['auth']);
 Route::post('invoices/{invoice}/status', [InvoiceController::class, 'updateStatus'])->name('invoices.updateStatus')->middleware(['auth']);
 Route::get('invoices/{invoice}/send', [InvoiceController::class, 'send'])->name('invoices.send')->middleware(['auth']);
@@ -41,6 +41,13 @@ Route::get('invoices/{invoice}/pay', [InvoiceController::class, 'pay'])->name('i
 Route::get('invoices/{invoice}/payment/success', [InvoiceController::class, 'paymentSuccess'])->name('invoices.payment.success');
 Route::get('invoices/{invoice}/payment/cancel', [InvoiceController::class, 'paymentCancel'])->name('invoices.payment.cancel');
 
+//transaction page routes
 Route::resource('transactions', TransactionController::class)->only(['index'])->middleware(['auth']);
+
+//login page routes
+Route::post('/logout', function () {
+    Auth::logout();
+    return redirect('/login');
+})->name('logout');
 
 require __DIR__.'/auth.php';
